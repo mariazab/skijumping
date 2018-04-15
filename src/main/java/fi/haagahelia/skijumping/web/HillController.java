7d738b70df0e035da327d3f7078ad8cdb08b3a39
Marysia Zabinska <marysia-zabinska@wp.pl>
2018-04-15 11:55:35 +0300

    Added twitter feed, latest results and top 3 standings, finilized styling

--- src/main/java/fi/haagahelia/skijumping/web/HillController.java
+++ src/main/java/fi/haagahelia/skijumping/web/HillController.java
@@ -3,6 +3,8 @@ package fi.haagahelia.skijumping.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import fi.haagahelia.skijumping.domain.Hill;
 import fi.haagahelia.skijumping.domain.HillRecord;
 import fi.haagahelia.skijumping.domain.HillRecordRepository;
 import fi.haagahelia.skijumping.domain.HillRepository;
+import fi.haagahelia.skijumping.domain.User;
+import fi.haagahelia.skijumping.domain.UserRepository;
 
 @Controller
 public class HillController {
@@ -26,9 +30,17 @@ public class HillController {
 	
 	@Autowired AthleteRepository athleteRepository;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	// Showing all hills with records
 	@RequestMapping("/hills")
 	public String showHills(Model model) {
+		Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+	    User user = userRepository.findByUsername(loggedInUser.getName());
+	    String name = user.getName();
+	    model.addAttribute("name", name);
+		
 		model.addAttribute("hills", repository.findAll());
 		/*model.addAttribute("records", recordRepository.findAll());*/
 		return "hills";
--- src/main/java/fi/haagahelia/skijumping/web/SkiJumpingController.java
+++ src/main/java/fi/haagahelia/skijumping/web/SkiJumpingController.java
@@ -1,5 +1,6 @@
 package fi.haagahelia.skijumping.web;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -11,10 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.skijumping.domain.AthleteRepository;
 import fi.haagahelia.skijumping.domain.Competition;
 import fi.haagahelia.skijumping.domain.CompetitionRepository;
+import fi.haagahelia.skijumping.domain.Result2018;
+import fi.haagahelia.skijumping.domain.Result2018Repository;
 import fi.haagahelia.skijumping.domain.User;
 import fi.haagahelia.skijumping.domain.UserRepository;
+import fi.haagahelia.skijumping.domain.WcStanding2018;
+import fi.haagahelia.skijumping.domain.WcStanding2018Repository;
 
 
 @Controller
@@ -26,16 +32,48 @@ public class SkiJumpingController {
 	@Autowired 
 	CompetitionRepository competitionRepository;
 	
+	@Autowired
+	AthleteRepository athleteRepository;
+	
+	@Autowired
+	Result2018Repository resultRepository;
+	
+	@Autowired
+	WcStanding2018Repository standingsRepository;
+	
 	@RequestMapping(value="/dashboard", method=RequestMethod.GET)
 	public String skijumping(Model model) {
 		//Get the name of the current user and pass it as a model
 		Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
 	    User user = userRepository.findByUsername(loggedInUser.getName());
 	    String name = user.getName();
+	    
+	    //Get next competition and pass it
 	    Competition competition = getNextCompetition();
+	    
+	    
+	    //Get the top 3 from last competition
+	    Competition lastCompetition = getLastCompetition();
+	    long competitionId = lastCompetition.getId();
+	    List<Result2018> results = resultRepository.findByCompetitionIdOrderByWcPoint(competitionId);
+	    List<Result2018> topThree = new ArrayList<Result2018>();
+		for(int i = 0; i < results.size(); i++) {
+			if(results.get(i).getWcPoint().getPosition() < 4) {
+				topThree.add(results.get(i));
+			}
+		}
+		
+		//Get top 3 from standings
+		List<WcStanding2018> standings = standingsRepository.findAllByOrderByPointsDesc();
+		List<WcStanding2018> wcTopThree = new ArrayList<WcStanding2018>();
+		for(int i = 0; i < 3; i++) {
+			wcTopThree.add(standings.get(i));
+		}
+		model.addAttribute("wcTopThree", wcTopThree);
+		model.addAttribute("lastCompetition", lastCompetition);
+	    model.addAttribute("topThree", topThree);
 	    model.addAttribute("competition", competition);
 	    model.addAttribute("name", name);
-		
 		return "dashboard";
     }
 	
@@ -52,6 +90,20 @@ public class SkiJumpingController {
 		}
 		return competition;
 		
+	}
+	
+	public Competition getLastCompetition() {
+		Date today = new Date();
+		Competition competition = new Competition();
+		List<Competition> competitions = (List<Competition>) competitionRepository.findAllByOrderById();
+		for(int i = (competitions.size() - 1);i > 0; i--) {
+			Date competitionDate = competitions.get(i).getDate();
+			if(today.after(competitionDate)) {
+				competition = competitions.get(i);
+				i = 0;
+				}
+		}
+		return competition;
 	}
 
 }
